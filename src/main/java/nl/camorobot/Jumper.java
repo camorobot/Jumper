e35a03f58579a7a05de7f9e65e42faec15cfa128
Kevin <camorobot@gmail.com>
2024-03-24 17:40:15 +0100

    game setup

---
+++ src/main/java/nl/camorobot/Jumper.java
@@ -0,0 +1,21 @@
+package nl.camorobot;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class Jumper extends YaegerGame
+{
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
